cc77e3d14294dfc271914d2a59bd4e624606ba1e
Your Name <you@example.com>
2023-03-02 18:27:32 +0530

    basic ui screens - home, login and register pages

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path("", index_frontend, name="index-frontend"),
+    path("login", login, name="login"),
+    path("register", register, name="register"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import pyrebase
 
 # Create your views here.
+
+def index_frontend(request):
+    return render(request, "app/frontend/index-frontend.html")
+
+def login(request):
+    return render(request, "app/frontend/page-login.html")
+
+def register(request):
+    return render(request, "app/frontend/page-register.html")
