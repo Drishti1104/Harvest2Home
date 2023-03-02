71492dc503e47aa655754b1698b30de63bf311bf
Your Name <you@example.com>
2023-03-02 21:36:19 +0530

    product and vendor screens added

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", index_frontend, name="index-frontend"),
     path("login", login, name="login"),
     path("register", register, name="register"),
+    path("shop-grid", shop_grid, name="shop-grid"),
+    path("product", product, name="product"),
+    path("vendors-grid", vendors_grid, name="vendors-grid"),
+    path("vendor-details", vendor_details, name="vendor-details"),
 ]
--- app/views.py
+++ app/views.py
@@ -11,3 +11,15 @@ def login(request):
 
 def register(request):
     return render(request, "app/frontend/page-register.html")
+
+def shop_grid(request):
+    return render(request, "app/frontend/shop-grid-left.html")
+
+def product(request):
+    return render(request, "app/frontend/shop-product-full.html")
+
+def vendors_grid(request):
+    return render(request, "app/frontend/vendors-grid.html")
+
+def vendor_details(request):
+    return render(request, "app/frontend/vendor-details.html")
