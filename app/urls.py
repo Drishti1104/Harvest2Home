0012eb4103bfe7f21e305a68a5288dd6332be1cf
Vansh Pahuja <vanshpahuja02@gmail.com>
2023-03-03 11:11:42 +0530

    updated firbase configuration

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,9 @@ from .views import *
 urlpatterns = [
     path("", index_frontend, name="index-frontend"),
     path("login", login, name="login"),
+    path("logout", logout, name="logout"),
     path("register", register, name="register"),
+    path("postregister", postregister, name="postregister"),
     path("shop-grid", shop_grid, name="shop-grid"),
     path("product", product, name="product"),
     path("vendors-grid", vendors_grid, name="vendors-grid"),
