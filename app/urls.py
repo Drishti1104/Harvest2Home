6bf1fd1225d7fb059691bb169c672f8b1e46a9a6
Your Name <you@example.com>
2023-03-03 20:51:50 +0530

    minor changes

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,9 @@ from .views import *
 
 urlpatterns = [
     path("", index_frontend, name="index-frontend"),
-    path("login", login, name="login"),
+    path("login-page", login_page, name="login-page"),
     path("logout", logout, name="logout"),
     path("register", register, name="register"),
-    path("postregister", postregister, name="postregister"),
     path("shop-grid", shop_grid, name="shop-grid"),
     path("product", product, name="product"),
     path("vendors-grid", vendors_grid, name="vendors-grid"),
--- app/views.py
+++ app/views.py
@@ -3,51 +3,27 @@ from django.contrib import messages,auth
 import pyrebase
 
 # Create your views here.
-config={
-	"apiKey": "AIzaSyDmCBzUXgbBiJZGI-aLBKdEynLrzQ4Xo6Y",
-    "authDomain": "http://harvest2home-bfcd6.firebaseapp.com",
-    "databaseURL": "https://harvest2home-bfcd6-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "harvest2home-bfcd6",
-    "storageBucket": "http://harvest2home-bfcd6.appspot.com",
-    "messagingSenderId": "9991712811",
-    "appId": "1:9991712811:web:270b0cdd4d91dff4ae3a6e",
-}
-firebase=pyrebase.initialize_app(config)
-authe = firebase.auth()
-database=firebase.database()
 
 def index_frontend(request):
     return render(request, "app/frontend/index-frontend.html")
 
-def login(request):
-    # harvesttohome = database.child('Data').child('Name').get().val()
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    try:
-        user = auth.sign_in_with_email_and_password(email,password)
-    except:
-        messages.success(request, "User does not exist. Please register.")
-        return redirect("register")
+# def login(request):
+#     # harvesttohome = database.child('Data').child('Name').get().val()
+#     email = request.POST.get('email')
+#     password = request.POST.get('password')
+#     try:
+#         user = authe.sign_in_with_email_and_password(email,password)
+#         return redirect("index-page")
+        
+#     except:
+#         messages.success(request, "User does not exist. Please register.")
+#         return redirect("register")
     
-    return render(request, "app/frontend/page-login.html",{"e":email})
+def login_page(request):
+    return render(request, "app/frontend/page-login.html")
 
 def register(request):
     return render(request, "app/frontend/page-register.html")
-
-def postregister(request):
-
-    name = request.POST.get('name')
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    try:
-        user = authe.create_user_with_email_and_password(email,password)
-    except:
-        messages.success(request, "Unable to register.")
-        return redirect("register")
-    uid = user['localId']
-    data={"name":name, "status":"1"}
-    database.child("users").child(uid).child("details").set(data)
-    return render(request, 'app/frontend/page-register.html')
 
 def logout(request):
     auth.logout(request)
